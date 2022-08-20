19552d04acd55b6673d7c5eba1df7ecf423fabd3
zssztii <92452454+zsuky2@users.noreply.github.com>
2022-08-20 14:13:55 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,68 @@
-
-
 document.getElementById("btn").addEventListener("click", function () {
 
     var megtakaritas = document.getElementById("megtakaritas").value;
+    megtakaritasInput = document.getElementById("megtakaritas");
     idoValaszto = document.getElementById("idoValaszto");
     i = idoValaszto.selectedIndex;
     elteltIdo = Number(idoValaszto.options[i].value);
     kerekpar = document.getElementById("kerekpar");
     telefon = document.getElementById("telefon");
     konzol = document.getElementById("konzol");
-    text = document.getElementById("eredmeny");
     forintHULocale = Intl.NumberFormat('hu-HU');
+    text = document.getElementById("eredmeny");
     elegPenz = document.getElementById("elegPenz");
     radio = document.getElementsByName("radio");
 
 
+    if (megtakaritas <= 0 || megtakaritas % 1 != 0) {
 
-    if (megtakaritas <= 0 || megtakaritas % 1 != 0 || idoValaszto.value == 0 || (kerekpar.checked == false && telefon.checked == false && konzol.checked == false)) {
+        megtakaritasInput.classList.add("border-danger");
+        alert("Hibás adatot adott meg! Figyeljen arra, hogy a megtakarítás csak pozitív egész szám lehet!");
+    } else if (idoValaszto.value == 0) {
+
+        idoValaszto.classList.add("border-danger");
+        alert("Nem választotta ki, hogy mennyi ideig spórolt!");
+    } else if (megtakaritas <= 0 || megtakaritas % 1 != 0 || idoValaszto.value == 0 || (kerekpar.checked == false && telefon.checked == false && konzol.checked == false)) {
+
         alert("Nem töltött ki minden mezőt vagy hibás adatot adott meg! Figyeljen arra, hogy a megtakarítás csak pozitív egész szám lehet!");
 
     } else {
 
-
         if (elteltIdo == 1) {
+
             var eredmeny = megtakaritas * 26;
-            text.innerHTML = "Az ön megtakarítása: " + forintHULocale.format(eredmeny) + " Forint";
 
         } else if (elteltIdo == 2) {
 
             var eredmeny = megtakaritas * 52;
-            text.innerHTML = "Az ön megtakarítása: " + forintHULocale.format(eredmeny) + " Forint";
-
 
         } else if (elteltIdo == 3) {
 
             var eredmeny = megtakaritas * 156;
-            text.innerHTML = "Az ön megtakarítása: " + forintHULocale.format(eredmeny) + " Forint";
+
         } else if (elteltIdo == 4) {
 
             var eredmeny = megtakaritas * 260;
-            text.innerHTML = "Az ön megtakarítása: " + forintHULocale.format(eredmeny) + " Forint";
+
         }
 
         if ((kerekpar.checked == true && eredmeny >= 100000) || (telefon.checked == true && eredmeny >= 80000) || (konzol.checked == true && eredmeny >= 150000)) {
 
+            megtakaritasInput.classList.remove("border-danger");
+            idoValaszto.classList.remove("border-danger");
+            elegPenz.classList.remove("text-danger");
+
+            text.innerHTML = "Az ön megtakarítása: " + forintHULocale.format(eredmeny) + " Forint";
             elegPenz.innerHTML = "<h3>Van elég pénze a vásárláshoz!</h3>"
+
         } else if ((kerekpar.checked == true && eredmeny < 100000) || (telefon.checked == true && eredmeny < 80000) || (konzol.checked == true && eredmeny < 150000)) {
 
+            megtakaritasInput.classList.remove("border-danger");
+            idoValaszto.classList.remove("border-danger");
+
+            text.innerHTML = "Az ön megtakarítása: " + forintHULocale.format(eredmeny) + " Forint";
             elegPenz.innerHTML = "<h3>Nincs elég pénze a vásárláshoz!</h3>"
+            elegPenz.classList.add("text-danger");
         }
     }
 })
